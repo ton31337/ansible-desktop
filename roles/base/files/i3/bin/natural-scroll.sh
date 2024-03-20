4c5dafd11b67e75aeb1c4763665e9c4aec636a94
Donatas Abraitis <donatas@opensourcerouting.org>
2024-03-20 08:01:15 +0200

    Manage mouse sensitivity/natural scrooling for every input (pointer) device
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- roles/base/files/i3/bin/natural-scroll.sh
+++ roles/base/files/i3/bin/natural-scroll.sh
@@ -2,10 +2,11 @@
 
 # Get id of touchpad and the id of the field corresponding to
 # natural scrolling
-id=`xinput list | grep pointer | grep -Ei "Razer|Mouse|Touchpad" | cut -d'=' -f2 | cut -d'[' -f1`
+for id in `xinput list | grep pointer | grep -Ei "Razer|Mouse|Touchpad" | cut -d'=' -f2 | cut -d'[' -f1`
+do
+	# Set the property to true
+	xinput --set-prop $id 'libinput Natural Scrolling Enabled' 1 2>/dev/null
 
-# Set the property to true
-xinput --set-prop $id 'libinput Natural Scrolling Enabled' 1 2>/dev/null
-
-# Sensitivity
-xinput --set-prop $id 'libinput Accel Speed' '-0.7' 2>/dev/null
+	# Sensitivity
+	xinput --set-prop $id 'libinput Accel Speed' '-0.7' 2>/dev/null
+done
